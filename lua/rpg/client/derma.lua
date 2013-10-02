cc5cd089adb057efd0bed29f06187cb0a103cf15
Aeomi <Aeomei@gmail.com>
2013-10-02 15:53:21 +0200

    Fix: Derma fix

--- lua/rpg/client/derma.lua
+++ lua/rpg/client/derma.lua
@@ -30,26 +30,26 @@ function CreateFonts( )
         weight =        250;
         antialias =     true;
         outline =       true;
-		shadow = 		false -- Test this
+		shadow = 		true -- Test this
     } )
 end 
+CreateFonts( )
 
-
-concommand.Add( "Ayu_Rpg_Derma_NameMenu" function( )
+concommand.Add( "Ayu_Rpg_Derma_NameMenu", function( )
 
 	/*----------|
 	|   Funct   |
 	|----------*/	
 	
+	NameField = "x"
 	local function SendNameToSv( )
 		if NameField:GetValue( ):match( "^[%a ]+$" ) != nil then
-			Msg( NameField:GetValue( ) .."\n" ) -- Q: Newline?
+			Msg( NameField:GetValue( ) .."\n" ) 
 			net.Start( "ClientName" )
 			net.WriteString( NameField:GetValue( ) )
 			net.SendToServer( )
 			DNameText = "Your name is being changed..."
 			DNameTextColorBool = true
-			timer.Simple( 2, function( ) MainMenu:SetVisible( false ) ) end
 		else
 			DNameTextColorBool = false
 			DNameText = ( "The Name '".. NameField:GetValue( ) .."' contains illegal characters \nAllowed characters range from A - Z" )
@@ -66,38 +66,50 @@ concommand.Add( "Ayu_Rpg_Derma_NameMenu" function( )
 	local MainMenu = vgui.Create( "DFrame" )
 	MainMenu:SetPos( ( ScrW( )/3)-300, ( ScrH( )/3 )-200 )
 	MainMenu:SetSize( 275, 100 )
-	MainMenu:SetTitle( "Name Selection Panel" )
+	MainMenu:SetTitle( "")--Name Selection Panel" )
 	MainMenu:SetBackgroundBlur( true )
 	MainMenu:SetVisible( false )
 	MainMenu:SetDraggable( true )
 	MainMenu:ShowCloseButton( false )
 	MainMenu:MakePopup( )
-	MainMenu:Paint = function( ) 
+	MainMenu.Paint = function( ) 
 		DNameText = ""
 		draw.RoundedBox( 8, 0, 0, MainMenu:GetWide( ), MainMenu:GetTall( ), Color( 75, 75, 75, 250 ) )
-		draw.DrawText( "Enter your Name:", "AyuMFont", 50, 74, Color( 75, 75, 200, 255 ), TEXT_ALIGN_CENTER )
-		draw.DrawText( "Name Selection Screen", "AyuSBFont", 75, 5, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER )
-		if DNameTextColorBool == false then DNameTextColor = Color( 200, 50, 75 ) else DNameTextColor = Color( 50, 75, 200 ) end
-		draw.DrawText( DNameText, "AyuLFont", 25, 35, DNameTextColor, TEXT_ALIGN_CENTER )
+		draw.DrawText( "Enter your Name:", "AyuSBFont", 50, 74, Color( 75, 75, 200, 255 ), TEXT_ALIGN_CENTER )
+		draw.DrawText( "Name Selection Screen", "AyuSBFont", 60, 5, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER )
+		if DNameTextColorBool == false then 
+			DNameTextColor = Color( 200, 50, 75 ) 
+		else 
+			DNameTextColor = Color( 50, 75, 200 ) 
+		end
+		draw.DrawText( DNameText, "AyuLBFont", 100, 35, DNameTextColor, TEXT_ALIGN_CENTER )
 	end
 	//-------------------//
 	
 	
 	//-- Close Button--//
 	local BtnClose = vgui.Create( "DButton", MainMenu ) 
+	timer.Create( "Dec1", 0.1, 10, function( ) BtnCloseTxt = BtnCloseTxt - 1 end )
+	timer.Stop( "Dec1" )
+	BtnCloseTxt = 10
 	BtnClose:SetPos( 237, 5 )
 	BtnClose:SetSize( 30, 17 )
-	BtnClose:SetFont( "Marlett" )
-	BtnClose:SetText( "r" )
-	BtnClose:Paint = function( )
-		draw.RoundedBox( 2, 0, 0, Frame:GetWide( ), Frame:GetTall( ), Color( 25, 25, 25, 255 ) )
+	local BtnCloseTglTxt = false
+	if !BtnCloseTglTxt then
+		BtnClose:SetFont( "Marlett" )
+		BtnClose:SetText( "r" )
+	end
+	BtnClose.Paint = function( )
+		draw.RoundedBox( 2, 0, 0, BtnClose:GetWide( ), BtnClose:GetTall( ), Color( 25, 25, 25, 255 ) )
+		if BtnCloseTglTxt then
+			BtnClose:SetFont( "AyuButtonFont" )
+			BtnClose:SetText( BtnCloseTxt )
+		end	
 	end
 	BtnClose.DoClick = function( )
-		local Dec = 3
-		timer.Create( "Dec1", 0.5, 3, function( ) Dec = Dec - 1 end )
-		BtnClose:SetFont( "AyuButtonFont" )
-		BtnClose:SetText( Dec )
-		timer.Simple( 1.5, function( ) MainMenu:SetVisible( false ) end )
+		BtnCloseTglTxt = true
+		timer.Start( "Dec1" )
+		timer.Simple( 1, function( ) MainMenu:SetVisible( false ) end )
 	end
 	//-----------------//
 	
@@ -108,11 +120,11 @@ concommand.Add( "Ayu_Rpg_Derma_NameMenu" function( )
 	BtnSubmit:SetSize( 50, 16 )
 	BtnSubmit:SetFont( "AyuButtonFont" )
 	BtnSubmit:SetText( "Send" )
-	BtnSubmit:Paint = function( )
+	BtnSubmit.Paint = function( )
 		draw.RoundedBox( 2, 0, 0, BtnSubmit:GetWide( ), BtnSubmit:GetTall( ), Color( 25, 25, 25, 255 ) )
 	end
 	BtnSubmit.DoClick = function( )
-		BtnSubmit:SetText( "Sending..." )
+		BtnSubmit:SetText( "Sending" )
 		timer.Simple( 0.5, function( )
 			SendNameToSv( )
 			MainMenu:SetVisible( false )
@@ -122,15 +134,12 @@ concommand.Add( "Ayu_Rpg_Derma_NameMenu" function( )
 	
 	
 	//-- Text Field --//
-	local NameField = vgui.Create( "DTextEntry", MainMenu )
+	NameField = vgui.Create( "DTextEntry", MainMenu )
 	NameField:SetPos( 95, 75 )
 	NameField:SetTall( 14 )
 	NameField:SetWide( 110 )
 	NameField:SetEnterAllowed( true )
-	NameField.OnEnter = SendNameToSv( )
-	NameField:Paint = function( )
-		draw.RoundedBox( 2, 0, 0, NameField:GetWide( ), NameField:GetTall( ), Color( 200, 200, 200, 255 ) )
-	end
+	NameField.OnEnter = function( ) SendNameToSv( ) end
 	//----------------//
 	
 	
